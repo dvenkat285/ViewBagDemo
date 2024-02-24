8a4a71fc5c91c706e053095757d4c86ea2bf571e
dvenkat285 <116060501+dvenkat285@users.noreply.github.com>
2024-02-24 22:07:35 +0530

    Add project files.

---
+++ ViewBagDemo/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViewBagDemo.Models;
+
+namespace ViewBagDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            ViewBag.Message = "Message from View Bag!!";
+            ViewBag.CurrentDate = DateTime.Now.ToLongDateString();
+
+            string[] fruits = { "Apple", "Banana", "Grapes", "Orange" };
+
+            ViewBag.FruitsArray = fruits;
+
+
+            ViewBag.SportsList = new List<string>()
+            {
+                "Cricket",
+                "Football",
+                "Hockey",
+                "Baseball"
+            };
+
+            Employee Anas = new Employee();
+
+            Anas.EmpId = 22;
+            Anas.EmpName = "Anas Khan";
+            Anas.EmpDesignation = "Manager";
+
+
+            ViewBag.Employee = Anas;
+            return View();
+        }
+    }
+}
---
+++ ViewBagDemo/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViewBagDemo.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string EmpDesignation { get; set; }
+
+    }
+}
